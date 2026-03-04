d00b62fb440be5edfb432dc6cf42e15acfd80eaa
OlaMarcus09 <olawalemarcus92@gmail.com>
2026-03-04 10:49:49 +0100

    fix: explicit prisma engine and build scripts for vercel

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
+  datasource: {
+    url: process.env.DATABASE_URL,
   },
 });
